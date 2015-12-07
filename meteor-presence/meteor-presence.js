63baf5b3b17448ee0316cb7a65efa2d1724b4053
StephenPrayogo <stephenprayogo@gmail.com>
2015-12-07 16:49:11 +1100

    Added support for account management
    Added account management stuffs. The presence software now keeps track
    of both anonymous and logged in users.

--- meteor-presence/meteor-presence.js
+++ meteor-presence/meteor-presence.js
@@ -1,19 +1,85 @@
 Presences = new Mongo.Collection('presences');
+Logged = new Mongo.Collection('logged');
+
+var trimInput = function(val){
+    return val.replace(/^\s*|\s*$/g, "");
+}
+
+var isValidPassword = function(val, field){
+    if (val.length >= 6){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+var isValidUsername = function(val, field){
+    if(val.length > 0){
+        return true;
+    } else{
+        return false;
+    }
+}
 
 Meteor.methods({
     changeName: function(name){
         Presences.update(this.connection.id, {$set: {username: name}});
-    }
+    },
+
+    removeAnon: function(){
+        Presences.remove(this.connection.id);
+    },
+
+    addLogged: function(username){
+        Logged.insert({
+            _id: this.connection.id,
+            username: username,
+            connectedOn: new Date(),
+            address: this.connection.clientAddress
+        });
+    },
+
+    removeLogged: function(){
+        Logged.remove(this.connection.id);
+    },
+
+    addAnon: function(){
+        Presences.insert({
+            _id: this.connection.id,
+            username: this.connection.id,
+            connectedOn: new Date(),
+            address: this.connection.clientAddress
+        });
+    },
+
+    customLogin: function(username){
+        Meteor.call("addLogged", username);
+        Meteor.call("removeAnon");
+    },
+
+    customLogout: function(){
+        Meteor.call("removeLogged");
+        Meteor.call("addAnon");
+    },
+
 });
 
 if (Meteor.isClient) {
 
     // Subscribe to "users" publication
+    Meteor.subscribe("presences");
     Meteor.subscribe("users");
 
+
     Template.body.helpers({
-        users: function(){
+        presences: function(){
             return Presences.find({});
+        }
+    });
+
+    Template.body.helpers({
+        registeredUsers: function(){
+            return Logged.find({});
         }
     });
 
@@ -34,12 +100,77 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.body.events({
+        "submit .logout" : function(event, t){
+            event.preventDefault();
+            console.log("LOGGING OUT");
+            // Retrieve the input field values
+            Meteor.logout(function(err){
+                if(err){
+                    console.log("logout failed");
+                } else {
+                    Meteor.call("customLogout");
+                }
+            });
+
+            return false;
+        }
+    });
+
+    Template.body.events({
+        "submit #login-form" : function(event, t){
+            event.preventDefault();
+            // Retrieve the input field values
+            var username = event.target.username.value;
+            username = trimInput(username);
+            var password = event.target.password.value;
+
+            Meteor.loginWithPassword(username, password, function(err){
+                if(err){
+                    console.log("failed");
+                } else{
+                    event.target.username.value = "";
+                    event.target.password.value = "";
+                }
+            });
+
+            return false;
+        }
+    });
+
+    Template.body.events({
+        "submit #register-form" : function(event, t){
+            event.preventDefault();
+            // Retrieve the input field values
+            var username = event.target.username.value;
+            username = trimInput(username);
+            var password = event.target.password.value;
+
+            if (isValidPassword(password) && isValidUsername(username)){
+                Accounts.createUser({username: username, password: password}, function(err){
+                    if(err){
+                        console.log("Account creation failed")
+                        // Inform user that account creation failed
+                    } else {
+                        console.log("Successfully created account")
+                        event.target.username.value = "";
+                        event.target.password.value = "";
+                        // Success. Account has been created and the user has logged in successfully.
+                    }
+                });
+            }
+
+            return false;
+        }
+    });
+
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
       // Clean up collection
       Presences.remove({});
+      Logged.remove({});
   });
 
   Meteor.onConnection(function(connection){
@@ -54,10 +185,17 @@ if (Meteor.isServer) {
       connection.onClose( () => {
           // Remove entry from collection
           Presences.remove(connection.id);
+          Logged.remove(connection.id);
           console.log("closed: " + connection.id);
       });
   });
 
+  //Accounts.onLogout
+  Accounts.onLogin(function(user){
+     Meteor.call('customLogin', user.user.username);
+  });
+
   // Publish collection
-  Meteor.publish("users", () => Presences.find({}));
+  Meteor.publish("presences", () => Presences.find({}));
+  Meteor.publish("users", () => Logged.find({}));
 }
